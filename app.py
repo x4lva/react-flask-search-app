fdc6095bfd86056c45f16085b9fa86ba3fa6eff0
x4lva <khevpa.dmytro@gmail.com>
2021-03-20 23:24:52 +0200

    Created flask server / Created parser

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 @app.route('/data', methods=['POST'])
 def hello_world():
     data = request.json.search
+
     return jsonify({"habr": qwery_parser.parse_habrahabr(data),
                     "stackoverflow": qwery_parser.parse_stackoverflow(data),
                     "toaster": qwery_parser.parse_toaster(data),
